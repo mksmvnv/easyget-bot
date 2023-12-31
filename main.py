c1ed368d04f5d6f9c62e651c10bc0bbd5b8e0fae
mksmvnv <mksmvnv@icloud.com>
2023-12-31 13:00:13 +0300

    Temp update

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
-import sys
-import logging
 import asyncio
+import logging
+import sys
 
-from aiogram import Bot, Dispatcher
-from app.handlers import router
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart
 from aiogram.enums import ParseMode
-from config import TOKEN
+from aiogram.utils.markdown import hbold
+from aiogram.types import Message
+from config import TOKEN, LOGISTICS, PL, FEE, EXCHANGE_RATE
+from app.keyboards import main_menu, back
+
+dp = Dispatcher()
 
 
-async def main() -> None:
+@dp.message(CommandStart())
+async def command_start_handler(message: Message):
+    await message.answer(f'Привет, {hbold(message.from_user.full_name)} 👋',
+                         reply_markup=main_menu)
+
+
+@dp.message(F.text.in_(['💸 Калькулятор']))
+async def question(message: Message):
+    await message.reply('Введите цену на товар в юанях:')
+
+
+@dp.message(lambda x: x.text.isdigit() and int(x.text) > 0)
+async def calcutator(message: Message):
+    user_input = int(message.text)
+    result = (user_input + PL) * \
+        EXCHANGE_RATE + (LOGISTICS + FEE)
+    await message.reply(
+        f'Цена товара в рублях: {
+            hbold(int(result))}₽\nБез учета доставки из Мск в Ваш город.',
+        reply_markup=back)
+
+
+@dp.message(F.text.in_(['🏃‍♂️ Назад']))
+async def handle_back(message: Message):
+    await message.reply(text='Возвращаюсь в главное меню!', reply_markup=main_menu)
+
+
+async def main():
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    dp = Dispatcher()
-    dp.include_router(router)
     await dp.start_polling(bot)
 
 
