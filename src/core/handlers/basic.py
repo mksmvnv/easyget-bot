42da57f49dc28acadb9e2237cf85d820514e9fe9
mksmvnv <mksmvnv@icloud.com>
2024-02-14 00:35:08 +0300

    Updated html style

--- src/core/handlers/basic.py
+++ src/core/handlers/basic.py
@@ -1,6 +1,12 @@
-from aiogram import Bot
 from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+from core.keyboards.reply import main_menu
 
 
 async def get_start(message: Message):
-    await message.answer(f'Привет, <b>{message.from_user.first_name}</b>!')
+    await message.answer(f'Привет, {hbold(message.from_user.first_name)}!',
+                         reply_markup=main_menu())
+
+
+async def get_hello(message: Message):
+    await message.reply(f'И тебе привет! Давай расчитаем стоимость твоего заказа?')
