cc642442883fa346324577079fb9e1f432f43048
mksmvnv <mksmvnv@icloud.com>
2024-02-14 14:13:02 +0300

    Added request for add user and reloaded get category keyboard

--- src/core/handlers/basic.py
+++ src/core/handlers/basic.py
@@ -1,20 +1,18 @@
-from aiogram import Bot
+# Created by @mksmvnv
+
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 from core.keyboards.reply import main_menu_keyboard
 from core.keyboards.inline import category_keyboard
+from core.utils.db_connect import Request
 
 
-async def get_start(message: Message):
+async def get_start(message: Message, request: Request):
+    await request.add_user(message.from_user.id, message.from_user.first_name)
     await message.answer(f'Привет, {hbold(message.from_user.first_name)}!',
                          reply_markup=main_menu_keyboard())
 
 
-async def get_hello(message: Message):
-    await message.reply(f'И тебе привет! Давай расчитаем стоимость твоего заказа?')
-    
-
-async def get_inline(message: Message, bot: Bot):
+async def get_category_keyboard(message: Message):
     await message.answer(f'Выберите категорию товара:',
                          reply_markup=category_keyboard())
-    
