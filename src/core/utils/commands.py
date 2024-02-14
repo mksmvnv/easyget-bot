60a4fa4d2c737bc4c61d9dde256d6d4e05dd3aeb
mksmvnv <mksmvnv@icloud.com>
2024-02-14 14:17:44 +0300

    Deleted unused commands

--- src/core/utils/commands.py
+++ src/core/utils/commands.py
@@ -1,30 +1,28 @@
+# Created by @mksmvnv
+
 from aiogram import Bot
 from aiogram.types import BotCommand, BotCommandScopeDefault
 
+
 async def set_commands(bot: Bot):
-       commands = [
-           BotCommand(
-               command='start',
-               description='Начало работы'
-           ),
-           BotCommand(
-               command='menu',
-               description='Главное меню'
-           ),
-           BotCommand(
-              command='category',
-              description='Категории'
-           ),
-           BotCommand(
-               command='help',
-               description='Поддержка'
-           ),
-           BotCommand(
-               command='cencel',
-               description='Назад'
-           )
-           
-       ]
-       
-       await bot.set_my_commands(commands, BotCommandScopeDefault())
-    
+    commands = [
+        BotCommand(
+            command='start',
+            description='Начало работы'
+        ),
+        BotCommand(
+            command='menu',
+            description='Главное меню'
+        ),
+        BotCommand(
+            command='help',
+            description='Поддержка'
+        ),
+        BotCommand(
+            command='cencel',
+            description='Назад'
+        )
+
+    ]
+
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
