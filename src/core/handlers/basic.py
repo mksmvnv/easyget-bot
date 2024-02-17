fd77dff0ce9433a1fb48269e39decc9a2f198154
mksmvnv <mksmvnv@icloud.com>
2024-02-17 23:34:51 +0300

    Added order, reviews, about, FAQ sections

--- src/core/handlers/basic.py
+++ src/core/handlers/basic.py
@@ -4,10 +4,10 @@ import re
 
 from random import randint
 
-from aiogram import Router, F
+from aiogram import Router, Bot, F
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
-from aiogram.utils.markdown import hbold, hcode
+from aiogram.utils.markdown import hbold, hcode, hitalic
 from aiogram.fsm.context import FSMContext
 
 from core.filters.admin import Admin
@@ -26,7 +26,7 @@ router = Router()
 
 @router.message(CommandStart())
 async def start_user_message(message: Message, request: Request):
-    await request.add_user(message.from_user.id, message.from_user.first_name)
+    await request.add_user(message.from_user.id, message.from_user.username)
     await message.answer(f'👋 Привет, {hbold(message.from_user.first_name)}! Вот и главное меню.',
                          reply_markup=main_menu_keyboard())
 
@@ -37,6 +37,13 @@ async def start_admin_message(message: Message):
                          reply_markup=main_menu_keyboard())
 
 
+@router.message(F.text == '⚡ Заказать')
+async def category_order_message(message: Message, state: FSMContext):
+    await state.set_state(Order.city)
+    await message.answer(f'Выберите город доставки:',
+                         reply_markup=inline.city_keyboard())
+
+
 @router.message(F.text == '🧮 Калькулятор')
 async def category_calculator_message(message: Message, state: FSMContext):
     await state.set_state(Calculation.logistics)
@@ -44,11 +51,34 @@ async def category_calculator_message(message: Message, state: FSMContext):
                          reply_markup=inline.category_keyboard())
 
 
-@router.message(F.text == '⚡ Заказать')
-async def category_order_message(message: Message, state: FSMContext):
-    await state.set_state(Order.city)
-    await message.answer(f'Выберите город доставки:',
-                         reply_markup=inline.city_keyboard())
+@router.message(F.text == '💰 Актуальный курс')
+async def current_exchange_rate_message(message: Message):
+    await message.answer(f'Актуальный курс CNY/RUB: {hcode(str(current_exchange_rate - 1) + '₽')}',
+                         reply_markup=inline.return_to_main_menu())
+
+
+@router.message(F.text == '📃 Отзывы')
+async def category_order_message(message: Message):
+    await message.answer(f'Все отзывы по ссылке ниже.',
+                         reply_markup=inline.show_reviews())
+
+
+@router.message(F.text == '📌 О нас')
+async def category_order_message(message: Message):
+    await message.answer('Кто мы?\n\n'
+                         'easyget. — это сервис доставки оригинальных товаров с площадки Poizon. '
+                         'Наша цель — помочь людям из РФ купить любой бренд, любую модель одежды и обуви, техники, '
+                         'которую они захотят, в эти непростые времена ограничений, '
+                         'когда во всех магазинах нашей необъятной страны пусто и безвкусно. '
+                         'Товар полностью оригинальный, проходит проверку Legit-Check. '
+                         'Цены ниже рыночных, благодаря воздействию внутреннего китайского рынка.',
+                         reply_markup=inline.return_to_main_menu())
+
+
+@router.message(F.text == '🛟 FAQ')
+async def category_order_message(message: Message):
+    await message.answer(f'Все инструкции и подробнее о нашей компании вы можете посмотреть в официальной группе по ссылке ниже.',
+                         reply_markup=inline.show_all_info())
 
 
 @router.message(Calculation.product)
@@ -59,31 +89,32 @@ async def calculation_message(message: Message, state: FSMContext):
         amount = calculator(int(data.get('product')), data.get('logistics'))
         await message.answer(f'Итоговая цена товара: {hcode(str(int(amount)) + '₽')}\n'
                              f'Без учета доставки из Москвы до вашего города.',
-                             reply_markup=inline.back_to_main_menu())
+                             reply_markup=inline.return_to_main_menu())
         await state.clear()
     else:
         await message.answer('Некорректное число. Введите еще раз.')
 
 
 @router.message(Order.link)
-async def order_message(message: Message, state: FSMContext):
+async def order_message(message: Message, bot: Bot, state: FSMContext):
     pattern = r"https://dw4\.co/t/A/[^ ]+"
     search = re.search(pattern, message.text)
     if search:
         link = search.group(0)
         await state.update_data(link=link)
         data = await state.get_data()
-        await message.answer(f'{hbold(message.from_user.first_name)}!\n'
-                             f'Ваш заказ № {hcode(randint(1, 1000))}\n'
-                             f'Ссылка на товар: {data.get('link')}\n'
-                             'Ожидайте сообщения от нашего менеджера.',
-                             reply_markup=inline.back_to_main_menu())
+        order_number = randint(1, 1000)
+        await message.answer(f'✅ {hbold(message.from_user.first_name)}, ваш заказ успешно оформлен!\n\n'
+                             f'Заказ №: {hcode(order_number)}\n'
+                             f'Логин: {hcode(message.from_user.username)}\n'
+                             f'Ссылка на товар: {hcode(data.get('link'))}\n\n'
+                             f'{hitalic(
+                                 'Ожидайте сообщения от нашего менеджера.')}',
+                             reply_markup=inline.return_to_main_menu())
         await state.clear()
+        order_info = f'Имя: {hbold(message.from_user.first_name)}\nЗаказ №: {hcode(order_number)}\nЛогин: '\
+            f'{hcode(message.from_user.username)}\nСсылка на товар: {
+                hcode(data.get('link'))}'
+        await bot.send_message(admin_id, order_info)
     else:
         await message.answer('Некорректная ссылка. Введите еще раз.')
-
-
-@router.message(F.text == '💰 Актуальный курс')
-async def current_exchange_rate_message(message: Message, state: FSMContext):
-    await message.answer(f'Актуальный курс CNY/RUB: {hcode(str(current_exchange_rate - 1) + '₽')}',
-                         reply_markup=inline.back_to_main_menu())
