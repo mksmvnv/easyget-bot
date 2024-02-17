e682c8bc822cd3da4404856a5041014b9bf8e043
mksmvnv <mksmvnv@icloud.com>
2024-02-17 23:35:39 +0300

    Added return to main menu button and select category for order

--- src/core/handlers/callback.py
+++ src/core/handlers/callback.py
@@ -1,23 +1,17 @@
 # Created by @mksmvnv
 
-from aiogram import Router, F
+from aiogram import Router
 from aiogram.types import CallbackQuery
 from aiogram.fsm.context import FSMContext
+from aiogram.utils.markdown import hbold
 
-from core.utils.callback_data import Category, City
+from core.utils.callback_data import Category, City, Pagination
 from core.utils.states import Calculation, Order
 from core.keyboards.inline import cancel_order
+from core.keyboards.reply import main_menu_keyboard
 
 
 router = Router()
-
-
-@router.callback_query(Category.filter(), Calculation.logistics)
-async def select_category_for_calculation(call: CallbackQuery, callback_data: Category, state: FSMContext):
-    await state.update_data(logistics=callback_data.logistics)
-    await state.set_state(Calculation.product)
-    await call.message.answer('Введите цену на товар в юанях:')
-    await call.answer()
 
 
 @router.callback_query(City.filter(), Order.city)
@@ -28,6 +22,15 @@ async def select_category_for_order(call: CallbackQuery, callback_data: City, st
     await call.answer()
 
 
-@router.callback_query(F.data == 'main_menu')
-async def back_to_main_menu(call: CallbackQuery):
+@router.callback_query(Category.filter(), Calculation.logistics)
+async def select_category_for_calculation(call: CallbackQuery, callback_data: Category, state: FSMContext):
+    await state.update_data(logistics=callback_data.logistics)
+    await state.set_state(Calculation.product)
+    await call.message.answer('Введите цену на товар в юанях:')
+    await call.answer()
+
+
+@router.callback_query(Pagination.filter())
+async def select_return_to_main_menu(call: CallbackQuery):
+    await call.message.answer(f'{hbold(call.from_user.first_name)}, возвращаемся в главное меню!', reply_markup=main_menu_keyboard())
     await call.answer()
