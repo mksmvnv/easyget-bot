0999872a95c9a8ae8188d03ab327f7a94a9f1c6e
mksmvnv <mksmvnv@icloud.com>
2024-02-17 23:38:26 +0300

    Added pagination and link classes

--- src/core/utils/callback_data.py
+++ src/core/utils/callback_data.py
@@ -6,3 +6,15 @@ from aiogram.filters.callback_data import CallbackData
 class Category(CallbackData, prefix='category'):
     name: str
     logistics: int
+
+
+class City(CallbackData, prefix='city'):
+    name: str
+
+
+class Pagination(CallbackData, prefix='pag'):
+    page: str
+
+
+class Link(CallbackData, prefix='link'):
+    path: str
