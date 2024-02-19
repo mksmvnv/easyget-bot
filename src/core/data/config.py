0b3b9c663f17c7d451f53334f1a678308cbf233a
mksmvnv <mksmvnv@icloud.com>
2024-02-19 20:03:25 +0300

    Moved config file to new direct

---
+++ src/core/data/config.py
@@ -0,0 +1,29 @@
+# Created by @mksmvnv
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Personal data
+
+bot_token = os.getenv('BOT_TOKEN')
+admin_id = os.getenv('ADMIN_ID')
+
+# Logistics prices
+
+sneakers_logistics = os.getenv('SNEAKERS_LOGISTICS')
+down_jackets_logistics = os.getenv('DOWN_JACKETS_LOGISTICS')
+other_logistics = os.getenv('OTHER_LOGISTICS')
+china_logistics = os.getenv('CHINA_LOGISTICS')
+fee = os.getenv('FEE')
+url = os.getenv('URL')
+
+# Data Base
+
+pguser = os.getenv('PGUSER')
+password = os.getenv('PASSWORD')
+database = os.getenv('DATABASE')
+host = os.getenv('HOST')
+port = os.getenv('PORT')
