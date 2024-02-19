3f1b8b0f46a112e0c0bb08731fb210569e7cc5a7
mksmvnv <mksmvnv@icloud.com>
2024-02-19 20:42:44 +0300

    Moved main file direct

---
+++ src/core/main.py
@@ -0,0 +1,48 @@
+# Created by @mksmvnv
+
+import asyncio
+import asyncpg
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+
+from handlers import basic, callback
+from middlewares.db_middleware import DbSession
+
+from data.config import bot_token, pguser, pgpassword, database, host, port
+
+
+async def create_pool():
+    return await asyncpg.create_pool(user=pguser,
+                                     password=pgpassword,
+                                     database=database,
+                                     host=host,
+                                     port=port,
+                                     command_timeout=60)
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - [%(levelname)s] - '
+                        '(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s')
+
+    bot = Bot(token=bot_token, parse_mode=ParseMode.HTML)
+    pool_connect = await create_pool()
+    dp = Dispatcher()
+
+    dp.include_routers(basic.router, callback.router)
+    dp.message.middleware.register(DbSession(pool_connect))
+
+    try:
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
